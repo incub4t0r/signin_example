870f324c8db4cb9222f3925505721513e0ce06dd
incub4t0r <dchung0812@gmail.com>
2022-05-16 02:08:30 +0000

    pagebase and updated css

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+email=""
+
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -10,18 +12,24 @@ def index():
 def signin():
     if request.method == "POST":
         try:
+            global email
             email = request.form["email"]
             password = request.form["password"]
             # print(email)
             # print(password)
-            return redirect(url_for("index"), code=302)
+            return redirect(url_for("home"), code=302)
         except:
             return render_template("signin.html")
     elif request.method == "GET":
         return render_template("signin.html")
     else:
         return render_template("signin.html")
-        
+
+@app.route("/home")
+def home():
+    print(f"email is {email}")
+    return render_template("home.html", username=email)
+
 @app.route("/register")
 def register():
     return "you are now attempting to register a new user"
